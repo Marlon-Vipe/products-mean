764d0e4e9398f7f3c578dcd84290861d291c1ace
Marlon Villalona <iamvillalona@gmail.com>
2024-10-29 19:58:20 -0400

    added models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,LoginComponent],
-  templateUrl: './login/login.component.html',
+  templateUrl: './components/login/login.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
