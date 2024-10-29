b66584e0bac39194185a2a2079c19823be97cb3b
Marlon Villalona <iamvillalona@gmail.com>
2024-10-29 19:35:58 -0400

    added services

---
+++ src/app/components/read-product/read-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-read-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './read-product.component.html',
+  styleUrl: './read-product.component.css'
+})
+export class ReadProductComponent {
+
+}
